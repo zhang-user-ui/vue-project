a8ad23466e890d2f73a9dfecc6bdb92903e001e5
zhang <3487488836@qq.com>
2025-09-17 19:59:13 +0800

    初始化仓库并处理 vue-examples2

---
+++ vue-examples2/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import CourseSelect from './components/CourseSelect.vue'
+</script>
+
+<template>
+  <CourseSelect />
+</template>
+
+<style scoped></style>
---
+++ vue-examples2/src/router/index.ts
@@ -0,0 +1,9 @@
+// src/router/index.ts
+import { createRouter, createWebHistory } from 'vue-router'
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    /* 路由配置 */
+  ]
+})
+export default router
